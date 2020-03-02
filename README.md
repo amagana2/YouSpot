570a3f3658dab887be7527108ddbd43399c3cca1
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2020-03-01 20:14:04 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# YouSpot
+
+### Overview
+- This app is a **Chrome extension** to allow you to create a playlist with songs you've listened to via YouTube.
+- With the click of a button you should be able to add the current song into your Spotify playlist.
+
+### Future Features
+- Should be able to edit playlist options.
+- Create multiple playlists. 
---
+++ content.js
@@ -0,0 +1,19 @@
+// content.js
+
+alert("YouSpot detected!");
+
+// Get into YouTube
+// Here is a guide: https://developers.google.com/youtube/v3/quickstart/js
+
+// Figure out the video you're currently watching.
+
+// Extract the name, or give prompt to enter the name of the song.
+
+// Get into Spotify
+
+// Does the Playlist exist? 
+
+// if so, add the song.
+// otherwise, create new playlist and then add the song.
+
+// Confirm!
