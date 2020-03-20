f36f9111f517b1f8814d845fb05cdfb0b951ef47
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2020-03-20 15:29:31 -0400

    We can now retrieve YouTube data using the API. Any video we're on will work.

--- popup.js
+++ popup.js
@@ -1,5 +1,27 @@
 // popup.js
-alert("Popup.js");
+/**
+ * This file controls the "popup" HTML's javascript. This does not do any processing of the page you're on.
+ * In order to process the current page (not the popup), you'll need to call 'executeScript' for content_script.js.
+ */
+
+// Open Settings Page.
+function getSettingsPage() {
+    // TODO: Implement settings page.
+}
+
+document.getElementById('settings_click').addEventListener('click', getSettingsPage);
+
+// Call Youtube API for video info.
+function getVideoInfo() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"}, function () {
+            chrome.tabs.sendMessage(tabs[0].id, tabs[0].url); // Send the current YouTube URL.
+        });
+    });
+}
+
+document.getElementById('button_click').addEventListener('click', getVideoInfo);
+
 
 // TODO: How should we get the song?
 /**
